f0baee44d00cf401f0295d3bf761405b6cc1efb2
juan.gonzalez22 <juan.gonzalez22@utp.edu.co>
2024-10-09 17:10:08 -0500

    Ya encuentra archivos
    Ya encuentra archivos y les pone el nombre

--- music-player-qt/mainwindow.cpp
+++ music-player-qt/mainwindow.cpp
@@ -7,9 +7,35 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    MPlayer = new QMediaPlayer();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_btnMute_clicked()
+{
+    if(IS_Muted == false)
+    {
+        IS_Muted = true;
+    }
+    else
+    {
+        IS_Muted = false;
+    }
+}
+
+
+void MainWindow::on_actionOpen_File_triggered()
+{
+    QString FileName = QFileDialog::getOpenFileName(this,tr("Select Audio File"),"",tr("MP3 Files (*.mp3)"));
+    MPlayer->setSource(QUrl(FileName));
+
+    QFileInfo fileInfo(FileName);
+    ui->fileName->setText(fileInfo.fileName());
+}
+
--- music-player-qt/mainwindow.h
+++ music-player-qt/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtMultimedia>
+#include <QtCore>
+#include <QtWidgets>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +21,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btnMute_clicked();
+
+    void on_actionOpen_File_triggered();
+
 private:
     Ui::MainWindow *ui;
+    bool IS_Muted = false;
+    QMediaPlayer *MPlayer;
 };
 #endif // MAINWINDOW_H
